92949b59fa51bea98b4362065bae7dc2793ab1ef
Martin Munilla <martinmunillas@gmail.com>
2020-10-13 23:22:12 -0300

    🧐 ProfileCompletionMiddleware implemented

--- munigram/settings.py
+++ munigram/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'munigram.middleware.ProfileCompletionMiddleware'
 ]
 
 ROOT_URLCONF = 'munigram.urls'
--- users/views.py
+++ users/views.py
@@ -7,42 +7,46 @@ from django.db.utils import IntegrityError
 
 from users.models import Profile
 
-def login_view(req):
-    if req.method == 'POST':
-        username = req.POST['username']
-        password = req.POST['password']
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-        user = authenticate(req, username=username, password=password)
+        user = authenticate(request, username=username, password=password)
         if user:
-            login(req, user)
+            login(request, user)
             return redirect('feed')
         else:
-            return render(req, 'users/login.html', {'error': 'Invalid password or username',})
-    return render(req, 'users/login.html')
+            return render(request, 'users/login.html', {'error': 'Invalid password or username',})
+    return render(request, 'users/login.html')
 
 @login_required
-def logout_view(req):
-    logout(req)
+def logout_view(request):
+    logout(request)
     return redirect('login')
 
-def signup_view(req):
-    if req.method == 'POST':
-        username = req.POST['username']
-        password = req.POST['password']
-        password_conf = req.POST['password_conf']
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_conf = request.POST['password_conf']
         if password != password_conf:
-            return render(req, 'users/signup.html', {'error': 'Password confirmation should match'})
+            return render(request, 'users/signup.html', {'error': 'Password confirmation should match'})
         try:
             user = User.objects.create_user(username=username, password=password)
         except IntegrityError:
-            return render(req, 'users/signup.html', {'error': 'That username already exists'})
-        user.email = req.POST['email']
-        user.first_name = req.POST['first_name']
-        user.last_name = req.POST['last_name']
+            return render(request, 'users/signup.html', {'error': 'That username already exists'})
+        user.email = request.POST['email']
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
         user.save()
 
         profile = Profile(user=user)
         profile.save()
         return redirect('login')
            
-    return render(req, 'users/signup.html')
+    return render(request, 'users/signup.html')
+
+@login_required
+def update_profile(request):
+    return render(request, 'users/update_profile.html')
